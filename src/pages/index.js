d89fe7dde9608fb0ac15af0b3d483b570c0562e7
Philip Harrison <philip@mailharrison.com>
2018-01-16 13:44:22 +0100

    Update copy

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ const IndexPage = () => (
   <div>
     <h2 className='heroIntro'>software engineer & product designer</h2>
     <h1 className='heroTitle'>i've built web products and helped people grow</h1>
-    <h1 className='heroTitle'>exploring ideas around how we think, feel, act, live, work and play together</h1>
+    <h1 className='heroTitle'>interested in how we think, feel, act, live, work and play together</h1>
     <p className='footer'>
       <a href='https://www.linkedin.com/in/feelepxyz/'>linkedin</a>
       <span>&nbsp;-&nbsp;</span>
