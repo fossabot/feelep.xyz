44a3814a6431132f267869d35fb340f350cec764
Philip Harrison <philip@mailharrison.com>
2018-05-01 15:33:06 +0200

    Update react-helmet and remove contentful

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,15 +1,3 @@
-const {
-  CONTENTFUL_SPACE_ID,
-  CONTENTFUL_ACCESS_TOKEN_PRODUCTION,
-  CONTENTFUL_ACCESS_TOKEN_PREVIEW,
-  CONTEXT
-} = process.env;
-
-// Use Contentful preview locally and for Netlify branch deploys
-const isProduction = CONTEXT === 'production';
-const CONTENTFUL_HOST = isProduction ? null : 'preview.contentful.com';
-const CONTENTFUL_ACCESS_TOKEN = isProduction ? CONTENTFUL_ACCESS_TOKEN_PRODUCTION : CONTENTFUL_ACCESS_TOKEN_PREVIEW;
-
 module.exports = {
   siteMetadata: {
     title: `feelep.xyz`,
@@ -17,14 +5,6 @@ module.exports = {
   plugins: [
     {
       resolve: `gatsby-plugin-react-helmet`,
-    },
-    {
-      resolve: `gatsby-source-contentful`,
-      options: {
-        spaceId: CONTENTFUL_SPACE_ID,
-        accessToken: CONTENTFUL_ACCESS_TOKEN,
-        host: CONTENTFUL_HOST,
-      },
     },
     { resolve: 'gatsby-transformer-remark' },
     { resolve: 'gatsby-plugin-netlify' }
